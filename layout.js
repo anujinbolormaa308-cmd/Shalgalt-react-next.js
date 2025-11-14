083c8db8d6b49477d137f5e3758025e8de5014a4
Anujka <anujinbolormaa308@gmail.com>
2025-11-14 20:27:59 +0800

    Delete layout.js

--- layout.js
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="mn">
-      <body className="bg-gray-100 p-6">{children}</body>
-    </html>
-  );
-}
